4583547c6f391695f86b31710de433227d1544da
Christine Borbon <christineborbon2002@gmail.com>
2024-10-13 22:39:20 +0800

    Create Song CRUD, Models, Views, Controllers, Etc.

---
+++ ASI.Basecode.Data/Models/Song.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Data.Models
+{
+    public class Song
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Album { get; set; }
+
+        public DateTime DateTimeCreated { get; set; } = DateTime.Now;
+    }
+}
